aec016b3a13177f24b5845530cede92313fe9397
Jeremy Paule Pereira <jeremy.workplace@gmail.com>
2023-03-08 10:24:13 -0300

    chore: Change api library for axios instead of only fetch

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,47 +1,36 @@
-import { User } from "@/types"
-import { baseUrl } from "../constants"
+import axios from "axios";
+import { User } from "@/types";
+import { baseUrl } from "../constants";
 
 const addMember = async (user: User): Promise<void> => {
-   
-    return fetch(`${baseUrl}/users`, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(user)
-    })
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(error => console.error(error))
-      
-}
+  try {
+    const response = await axios.post(`${baseUrl}/users`, user);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-    const getMembers = async (): Promise<User[]> => {
-    return fetch(`${baseUrl}/users`)
-      .then(response => response.json())
-  };
-  
-  const getMembersById = async (id: number): Promise<User> => {
-    return fetch(`${baseUrl}/users/${id}`)
-      .then(response => response.json())
-  };
-  
-  const updateMembers = async (id: string, user: User): Promise<User> => {
-    return fetch(`${baseUrl}/users/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(user)
-    })
-      .then(response => response.json())
-  };
-  
-  const deleteMembers = async (id: string): Promise<void> => {
-    return fetch(`${baseUrl}/users/${id}`, {
-      method: 'DELETE'
-    })
-      .then(() => undefined);
-  };
+const getMembers = async (): Promise<User[]> => {
+  const response = await axios.get<User[]>(`${baseUrl}/users`);
+  return response.data;
+};
 
-export { getMembers, getMembersById, addMember, updateMembers, deleteMembers }
+const getMembersById = async (id: number): Promise<User> => {
+  const response = await axios.get<User>(`${baseUrl}/users/${id}`);
+  return response.data;
+};
+
+const updateMembers = async (id: string, user: User): Promise<User> => {
+  const response = await axios.put<User>(
+    `${baseUrl}/users/${id}`,
+    user
+  );
+  return response.data;
+};
+
+const deleteMembers = async (id: string): Promise<void> => {
+  await axios.delete(`${baseUrl}/users/${id}`);
+};
+
+export { getMembers, getMembersById, addMember, updateMembers, deleteMembers };
